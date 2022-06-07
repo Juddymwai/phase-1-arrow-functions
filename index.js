4abb21ccb6e8e5c173ecc5af6c5a1ca958d9f08f
Judy Mwai <juddiemukami@gmail.com>
2022-06-08 00:46:51 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function(){
+    return 2000/100;
+}
+divide ()
+
+ let square = (parameter1) => {
+    return  parameter1 * parameter1;
+
+ }
+ let add = (num1, num2) =>{
+
+    return num1+ num2;
+ }
+ add()
